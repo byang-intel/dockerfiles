bedf2b9415e0ba6337e654a6a82771d24d9c859c
Bin Yang <bin.yang@intel.com>
2019-03-04 14:27:54 +0800

    add build.sh and test.sh
    Signed-off-by: Bin Yang <bin.yang@intel.com>

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+export PS1='\[\e[38;5;39m\]\u\[\e[0m\]@\[\e[38;5;208m\]\H \[\e[38;5;39m\]\w \[\e[38;5;39m\]$ \[\e[0;0m\]'
+
+#search local DISPLAY: ps e | grep -Po " DISPLAY=[\.0-9A-Za-z:]* " | sort -u
+if [ -n "$DISPLAY" ]; then
+	xhost +local:root
+fi
+
+sudo docker run --rm=true \
+	--network host --env PS1 --env http_proxy --env https_proxy \
+	--env DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
+	-v $PWD/share:/share -it $@
