8efa8b7af9e8ebf1cd9cf601b2bd6ac608386eb1
Bin Yang <byang@intel.com>
2019-04-01 13:57:34 +0800

    edgex-pi-demo-arm64: ffserver
    Signed-off-by: Bin Yang <byang@intel.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-sudo -E docker build -t $1 --build-arg http_proxy --build-arg https_proxy -f $1/Dockerfile .
+sudo -E docker build -t $2 --build-arg http_proxy --build-arg https_proxy -f $1 .
--- edgex-pi-demo-arm64/edgex-pi-demo/edgex-mqtt-command.py
+++ edgex-pi-demo-arm64/edgex-pi-demo/edgex-mqtt-command.py
@@ -7,9 +7,11 @@ import paho.mqtt.client as mqtt
 
 def camera_handle(command):
     if command == "camera_on" and not camera_handle.proc:
-        camera_handle.proc = subprocess.Popen(['python3 /opt/edgex-pi-demo/camera2http.py'], shell=True)
+#        camera_handle.proc = subprocess.Popen(['python3 /opt/edgex-pi-demo/camera2http.py'], shell=True)
+        camera_handle.proc = subprocess.Popen(['/usr/local/bin/ffmpeg -f v4l2 -i /dev/video0 http://localhost:49990/camera.ffm'], shell=True)
     elif command == "camera_off" and camera_handle.proc: 
-        os.system("kill `cat /tmp/camera2http.pid`")
+#        os.system("kill `cat /tmp/camera2http.pid`")
+        os.system("killall ffmpeg")
         camera_handle.proc.terminate()
         camera_handle.proc.wait()
         camera_handle.proc = None
