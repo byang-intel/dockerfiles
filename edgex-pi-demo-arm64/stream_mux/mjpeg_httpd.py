7e3fb1f984ab53de9256c1a8035f7e32ef6980d1
Bin Yang <byang@intel.com>
2019-04-24 12:08:36 +0800

    edgex-pi-demo-arm64/stream_mux: add fps control
    Signed-off-by: Bin Yang <byang@intel.com>

--- edgex-pi-demo-arm64/stream_mux/mjpeg_httpd.py
+++ edgex-pi-demo-arm64/stream_mux/mjpeg_httpd.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 from time import sleep
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['get'])
 def index():
-    return Response(gen(),
+    fps_max = request.args.get('fps_max', 30)
+    delay = 1/float(fps_max)
+    return Response(gen(delay),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-def gen():
+def gen(delay):
     while True:
         out = open("out.jpg", "rb")
         if out:
@@ -21,6 +23,7 @@ def gen():
         if frame:
             yield (b'--frame\r\n' 
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        sleep(delay)
          
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=49990, threaded=True, debug=True)
